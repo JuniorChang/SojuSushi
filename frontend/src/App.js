941dbb7eb867bfca0bd50f74ec310b99d43d7895
junior chang <changjiale@gmail.com>
2022-05-01 18:38:45 +0800

    navbar and fontawesome added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className="py-3">
         <Container>
           <h1>Welcome To Soju Sushi!</h1>
         </Container>
