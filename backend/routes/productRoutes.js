7eded94ac552df2e1f4bfb77c52cc4ce3e8eb0c4
junior chang <changjiale@gmail.com>
2022-05-05 19:47:39 +0800

    added jwt

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,35 +1,11 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
 const router = express.Router();
-import Product from "../models/productModel.js";
+import {
+  getProducts,
+  getProductbyId,
+} from "../controllers/productController.js";
 
-// @desc   Get all products
-// @route  GET /api/products
-// @accesss  Public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    // throw new Error("nope");
-    res.json(products);
-  })
-);
-
-// @desc   Get single product
-// @route  GET /api/products/:id
-// @accesss  Public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not found");
-    }
-  })
-);
+router.route("/").get(getProducts);
+router.route("/:id").get(getProductbyId);
 
 export default router;
