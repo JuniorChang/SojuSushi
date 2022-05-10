dfa1930d0e3d3e5ab685b650ec28f2e736472010
junior chang <changjiale@gmail.com>
2022-05-10 11:53:36 +0800

    setting up orders

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Form, Button, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+import { savePaymentMethod } from "../actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps";
+import { useNavigate } from "react-router-dom";
+
+const PaymentScreen = ({ history }) => {
+  const navigate = useNavigate();
+
+  //getting shipping address
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  //checking if there is shipping address, they should have, otherwise
+  //redirect back to shipping screen
+  if (!shippingAddress) {
+    navigate("/shipping");
+  }
+
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate("/placeorder");
+  };
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label as="legend">Select Method</Form.Label>
+
+          <Col>
+            <Form.Check
+              type="radio"
+              label="PayPal or Credit Card"
+              id="PayPal"
+              name="paymentMethod"
+              value="PayPal"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+
+            {/* to add other options for payment eg.STRIPE, uncomment the following lines */}
+            <Form.Check
+              type="radio"
+              label="Stripe (coming soon)"
+              id="stripe"
+              name="paymentMethod"
+              value="stripe"
+              disabled
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+          </Col>
+        </Form.Group>
+
+        <Button type="submit" variant="primary" className="my-3">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default PaymentScreen;
